3049f5f2679539a1e5b7c8ce4b91e6aafbef57a7
Tankiso Montse <tmontse16@gmail.com>
2025-09-09 01:48:11 +0200

    imported a playfair display font from google fonts and saved its tailwind variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "@/globals.css"
 
 const geistSans = Geist({
@@ -9,6 +9,13 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
   subsets: ["latin"],
 });
 
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-full`}
+        className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} antialiased h-screen w-full`}
       >
         {children}
       </body>
