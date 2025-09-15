45f77efee3112951d9477c530ae4200a568bd281
Tankiso Montse <tmontse16@gmail.com>
2025-09-15 16:51:56 +0200

    added a menu-bar in the navbar section of the header

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Hero() {
   return (
     <section className="relative w-full h-full bg-amber-950">
-      <div className='flex flex-col justify-between h-full w-full absolute p-10'>
-        <h1 className='font-playfair text-8xl font-semibold tracking-tighter'>Furever Home</h1>
+      <div className='flex flex-col justify-between h-full w-full absolute p-10 z-10'>
+        <div className='flex justify-between pr-8'>
+          <h1 className='font-playfair text-8xl font-semibold tracking-tighter'>Furever Home</h1>
+          <Image src={'/icons/menu-bar.svg'} alt='menu-bar' width={50} height={50} className='cursor-pointer' />
+        </div>
         <p className='max-w-lg self-end'>Every pet deserves a loving home, and every person deserves the joy of a loyal companion. With thousands of cats, dogs, and other furry friends waiting, your perfect match could be just a click away.</p>
       </div>
       <video
