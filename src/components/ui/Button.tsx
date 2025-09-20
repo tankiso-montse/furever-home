91e45346c0ccb086dc4daa79184f17c8ee0617c6
Tankiso Montse <tmontse16@gmail.com>
2025-09-20 16:57:05 +0200

    added a extraClasses prop to extend styling for the custom button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  extraClasses?: string;
+}
+ 
 
-
-function Button({ children }: ButtonProps) {
+function Button({ children, extraClasses, ...props }: ButtonProps) {
   return (
-    <button className={twMerge('bg-primary text-secondary border border-secondary rounded-3xl font-inter font-medium px-8 py-2 h-12 hover:bg-secondary hover:text-primary transition-all duration-300 cursor-pointer')}>
+    <button {...props} className={twMerge('bg-primary text-secondary border border-secondary rounded-3xl font-inter font-medium px-8 py-2 h-12 hover:bg-tertiary hover:border-tertiary hover:text-primary transition-all duration-300 cursor-pointer', extraClasses)}>
       {children}
     </button>
   )
